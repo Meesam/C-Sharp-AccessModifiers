e52b3986dfd72d50bef5869ccb53399572b479f8
meesam <meesam.engineer@gmail.com>
2023-11-21 10:06:34 +0530

    add Protect and Protect Internal modifier

--- DemoLibrary/DemoAccess.cs
+++ DemoLibrary/DemoAccess.cs
@@ -19,4 +19,16 @@ public class DemoAccess
     {
         Console.WriteLine("This is Public Modifier Demo");
     }
+
+    // Can call only with in the class or from the class which is inherit from this class
+    protected void ProtectedDemo()
+    {
+        Console.WriteLine("This is Protected Modifier Demo");
+    }
+
+    // Can call from within the same assembly or Project and from class which is in different assembly or project that is inherit from the base class 
+    protected internal void ProtectedInternalDemo()
+    {
+        Console.WriteLine("This is Protected internal Modifier Demo");
+    }
 }
